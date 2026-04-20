e74c9b4724a6798f55267f71b0bf0011d2817adc
Jonathan Harun <jojoharun1@gmail.com>
2026-04-19 22:50:15 -0400

    Add About Us page

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,10 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+# ⚠️ GANTI <your-app-url> dengan URL dari tombol Django Application
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'jojoharun1-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+
+CSRF_TRUSTED_ORIGINS = ['https://jojoharun1-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -61,7 +63,9 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/static')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,5 +138,7 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = []
-
+# Tambahan agar Django bisa baca frontend static
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/static')
+]
